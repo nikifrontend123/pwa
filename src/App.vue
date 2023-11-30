67b45d85cec8de9a98379fcce1062a902183722c
nikhil fpaipl <frontendjr1@fpaipl.com>
2023-11-30 18:04:06 +0530

    installPrompt Done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button v-if="showInstallButton" @click="installApp">Install App</button>
+
 </template>
 
 <script>
@@ -8,8 +10,59 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      deferredPrompt: null,
+      showInstallButton: false,
+      showInstallPopup: false,
+    };
+  },
   components: {
     HelloWorld
+  },
+  mounted() {
+    // Delay the installation prompt by 1 second
+    setTimeout(() => {
+      this.showInstallPopup = true;
+    }, 1000);
+
+    // Add event listener for beforeinstallprompt
+    window.addEventListener('beforeinstallprompt', this.handleInstallPrompt);
+  },
+  unmounted() {
+    // Remove event listener when the component is unmounted
+    window.removeEventListener('beforeinstallprompt', this.handleInstallPrompt);
+  },
+  methods: {
+    handleInstallPrompt(event) {
+      // Prevent the default behavior to show the browser's install prompt
+      event.preventDefault();
+
+      // Store the event for later use
+      this.deferredPrompt = event;
+
+      // Show the install button
+      this.showInstallButton = true;
+    },
+    installApp() {
+      if (this.deferredPrompt) {
+        // Show the browser's install prompt
+        this.deferredPrompt.prompt();
+
+        // Wait for the user to respond to the prompt
+        this.deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+          } else {
+            console.log('User dismissed the install prompt');
+          }
+
+          // Reset the deferredPrompt and hide the install button
+          this.deferredPrompt = null;
+          this.showInstallButton = false;
+        });
+      }
+    },
   }
 }
 </script>
