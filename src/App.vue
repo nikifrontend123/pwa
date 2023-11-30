82ded9866722dfe45534fc6d180a6e75d7bc84d0
nikhil fpaipl <frontendjr1@fpaipl.com>
2023-11-30 18:50:25 +0530

    popup test2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <button class="btn btn-info w-100" v-if="showInstallButton" @click="installApp">Install App</button>
+  <button v-if="showInstallButton" @click="handleInstallButtonClick">Install App</button>
+  <div v-if="showInstallPopup" class="install-popup">
+    <div class="install-popup-content">
+      <p>Do you want to install this app?</p>
+      <button @click="installApp">Install</button>
+      <button @click="dismissInstall">Dismiss</button>
+    </div>
+  </div>
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
@@ -13,56 +20,51 @@ export default {
     return {
       deferredPrompt: null,
       showInstallButton: false,
-      showInstallPopup: false,
     };
   },
   components: {
     HelloWorld
   },
-  mounted() {
-    // Delay the installation prompt by 1 second
-    setTimeout(() => {
-      this.showInstallPopup = true;
-    }, 1000);
-
-    // Add event listener for beforeinstallprompt
+  created() {
     window.addEventListener('beforeinstallprompt', this.handleInstallPrompt);
+    // this.installApp(); // Remove this line, as we want to show the button only when the user clicks it
   },
   unmounted() {
-    // Remove event listener when the component is unmounted
     window.removeEventListener('beforeinstallprompt', this.handleInstallPrompt);
   },
   methods: {
-    handleInstallPrompt(event) {
-      // Prevent the default behavior to show the browser's install prompt
-      event.preventDefault();
+    // ... your existing methods ...
 
-      // Store the event for later use
-      this.deferredPrompt = event;
-
-      // Show the install button
-      this.showInstallButton = true;
+    showInstallAlert() {
+      // Set the flag to true to show the install popup
+      this.showInstallPopup = true;
     },
+
     installApp() {
-      if (this.deferredPrompt) {
-        // Show the browser's install prompt
-        this.deferredPrompt.prompt();
+      // If the user clicks "Install," show the browser's install prompt
+      this.deferredPrompt.prompt();
 
-        // Wait for the user to respond to the prompt
-        this.deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the install prompt');
-          } else {
-            console.log('User dismissed the install prompt');
-          }
+      // Wait for the user to respond to the prompt
+      this.deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
 
-          // Reset the deferredPrompt and hide the install button
-          this.deferredPrompt = null;
-          this.showInstallButton = false;
-        });
-      }
+        // Reset the deferredPrompt
+        this.deferredPrompt = null;
+
+        // Close the install popup
+        this.showInstallPopup = false;
+      });
     },
-  }
+
+    dismissInstall() {
+      // If the user clicks "Dismiss," close the install popup
+      this.showInstallPopup = false;
+    },
+  },
 }
 </script>
 
@@ -74,5 +76,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.install-popup {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #fff;
+  padding: 20px;
+  border: 1px solid #ccc;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.install-popup-content {
+  text-align: center;
+}
+
+.install-popup button {
+  margin: 10px;
+  padding: 8px 16px;
+  cursor: pointer;
 }
 </style>
