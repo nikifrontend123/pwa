3655005acc5d3e2587cc7427bc028419fde48891
nikhil fpaipl <frontendjr1@fpaipl.com>
2023-11-30 16:45:34 +0530

    dd

--- vue.config.js
+++ vue.config.js
@@ -15,33 +15,33 @@ module.exports = defineConfig({
       maskIcon: 'img/logo.png',
       msTileImage: 'img/logo.png',
     },
-    // manifestOptions: {
-    //   name: "Fabricator",
-    //   short_name: "Fabricator",
-    //   description: "Fabricator",
-    //   start_url: "./",
-    //   display: "standalone",
-    //   background_color: "#ffffff",
-    //   theme_color: "#000000",
-    //   orientation: "portrait",
-    //   "icons": [
-    //     {
-    //       "src": "img/logo.png",
-    //       "sizes": "72x72",
-    //       "type": "image/png"
-    //     },
-    //     {
-    //       "src": "img/logo.png",
-    //       "sizes": "96x96",
-    //       "type": "image/png"
-    //     } 
-    //   ],
-    //   workboxPluginMode: 'InjectManifest',
-    //   workboxOptions: {
-    //     swSrc: 'src/sw.js', // path to your customized service worker file
-    //     swDest: 'service-worker.js', // output path in the dist folder
-    //   },
-    //   // Add more options as needed
-    // }, 
+    manifestOptions: {
+      name: "Fabricator",
+      short_name: "Fabricator",
+      description: "Fabricator",
+      start_url: "./",
+      display: "standalone",
+      background_color: "#ffffff",
+      theme_color: "#000000",
+      orientation: "portrait",
+      "icons": [
+        {
+          "src": "img/logo.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "img/logo.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        } 
+      ],
+      workboxPluginMode: 'InjectManifest',
+      workboxOptions: {
+        swSrc: 'src/sw.js', // path to your customized service worker file
+        swDest: 'service-worker.js', // output path in the dist folder
+      },
+      // Add more options as needed
+    }, 
   }
 })
