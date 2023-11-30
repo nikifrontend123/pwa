496d029aa206312b4faf09df8abc00017b6b21ad
nikhil fpaipl <frontendjr1@fpaipl.com>
2023-11-30 18:50:11 +0530

    popup test1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <div v-if="showInstallPopup" class="install-popup">
-    <div class="install-popup-content">
-      <p>Do you want to install this app?</p>
-      <button @click="installApp">Install</button>
-      <button @click="dismissInstall">Dismiss</button>
-    </div>
-  </div>
-  <!-- <button class="btn btn-info w-100" v-if="showInstallButton" @click="installApp">Install App</button> -->
+  <button class="btn btn-info w-100" v-if="showInstallButton" @click="installApp">Install App</button>
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
@@ -19,23 +12,24 @@ export default {
   data() {
     return {
       deferredPrompt: null,
-      // showInstallButton: false,
-      // showInstallPopup: false,\
+      showInstallButton: false,
       showInstallPopup: false,
     };
   },
   components: {
     HelloWorld
   },
-  created() {
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-      window.addEventListener('beforeinstallprompt', this.handleInstallPrompt);
-      console.log('Event listener added for beforeinstallprompt');
-    } else {
-      console.warn('Service workers or PushManager not supported');
-    }
+  mounted() {
+    // Delay the installation prompt by 1 second
+    setTimeout(() => {
+      this.showInstallPopup = true;
+    }, 1000);
+
+    // Add event listener for beforeinstallprompt
+    window.addEventListener('beforeinstallprompt', this.handleInstallPrompt);
   },
   unmounted() {
+    // Remove event listener when the component is unmounted
     window.removeEventListener('beforeinstallprompt', this.handleInstallPrompt);
   },
   methods: {
@@ -46,39 +40,29 @@ export default {
       // Store the event for later use
       this.deferredPrompt = event;
 
-      // Show a browser-style alert immediately
-      this.showInstallAlert();
+      // Show the install button
+      this.showInstallButton = true;
     },
-    showInstallAlert() {
-      // Set the flag to true to show the install popup
-      this.showInstallPopup = true;
-    },
-
     installApp() {
-      // If the user clicks "Install," show the browser's install prompt
-      this.deferredPrompt.prompt();
+      if (this.deferredPrompt) {
+        // Show the browser's install prompt
+        this.deferredPrompt.prompt();
 
-      // Wait for the user to respond to the prompt
-      this.deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
+        // Wait for the user to respond to the prompt
+        this.deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+          } else {
+            console.log('User dismissed the install prompt');
+          }
 
-        // Reset the deferredPrompt
-        this.deferredPrompt = null;
-
-        // Close the install popup
-        this.showInstallPopup = false;
-      });
+          // Reset the deferredPrompt and hide the install button
+          this.deferredPrompt = null;
+          this.showInstallButton = false;
+        });
+      }
     },
-
-    dismissInstall() {
-      // If the user clicks "Dismiss," close the install popup
-      this.showInstallPopup = false;
-    },
-  },
+  }
 }
 </script>
 
@@ -90,25 +74,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-.install-popup {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background: #fff;
-  padding: 20px;
-  border: 1px solid #ccc;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-.install-popup-content {
-  text-align: center;
-}
-
-.install-popup button {
-  margin: 10px;
-  padding: 8px 16px;
-  cursor: pointer;
 }
 </style>
