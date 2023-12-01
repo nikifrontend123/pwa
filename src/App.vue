26bd3faabc9d7ad815fc3c2be23974388a77ea28
nikhil fpaipl <frontendjr1@fpaipl.com>
2023-12-01 16:14:36 +0530

    Version 1.25

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="p-2 text-bg-dark w-100">
-      Version 1.15
+      Version 1.25
     </div>
     <img alt="Vue logo" src="./assets/logo.png">
     <div v-if="showInstallPopup" class="install-popup">
@@ -13,7 +13,7 @@
     </div>
     <!-- Example deep links -->
     <!-- <a  href="fabricator://">Open PWA App</a> -->
-    <a  href="scheme:host">Open PWA App</a>
+    <a  href="/radiant-mermaid-5d059e.netlify.app">Open PWA App</a>
     
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,29 +18,35 @@ export default {
   },
   methods: {
     checkPwaStatus() {
+      // Check if the browser supports service workers and the app is not in standalone mode
       if ('serviceWorker' in navigator && !window.matchMedia('(display-mode: standalone)').matches) {
         this.showPrompt = true;
       }
     },
-    redirectToPwa() {
-      // Logic to redirect to the PWA
-      // This might involve navigating to a URL or providing instructions
-
-    },
     redirectToApp() {
-      console.log('redirecting')
-      // Try to open the app using deep linking
+      console.log('Redirecting to the app');
+      
+      // Check if the browser supports service workers and Notifications
       if ('serviceWorker' in navigator && 'Notification' in window) {
         navigator.serviceWorker.ready.then((registration) => {
+          // Check if navigation preload is available
           if (registration.navigationPreload) {
             // PWA is installed, redirect to PWA
             window.location.href = '/radiant-mermaid-5d059e.netlify.app';
+          } else {
+            // Fallback for browsers that do not support navigation preload
+            // You might want to provide instructions or navigate to a specific URL
+            window.location.href = '/fallback-url';
           }
         });
+      } else {
+        // Fallback for browsers that do not support service workers or Notifications
+        // You might want to provide instructions or navigate to a specific URL
+        window.location.href = '/fallback-url';
       }
-      // window.location.href = '/netlify.com';
-      // window.location.replace = ('pwa://radiant-mermaid-5d059e.netlify.app/');
 
+      // Optionally, you can use a deep link or a custom URL scheme to open the app
+      // window.location.href = 'your-custom-scheme://path/to/content';
     },
     dismissPrompt() {
       this.showPrompt = false;
